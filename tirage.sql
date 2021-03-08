e12010cc1612ddd2c41a7983b49f86e60b572069
l3miage-kimoura <kimahmedislm@gmail.com>
2021-03-08 18:42:30 +0100

    I added a txt file where we're gonna add the relations

--- tirage.sql
+++ tirage.sql
@@ -3,14 +3,16 @@ drop table FichierImage ;
 drop table EstProprioDe ; 
 drop table Adresse ;
 drop table Impression ;
+drop table Livraison;
+drop table Commande;
 
 
 create table Client (
     adrMail varchar(100) primary key,
     nom varchar(100),
     prenom text[],
-    adrPostale varchar(100),
-    mdp varchar(100) REFERENCES Adresse
+    Foreign key adrPostale varchar(100) REFERENCES Adresse ,
+    mdp varchar(100) 
 )
 
 create table FichierImage (
@@ -18,7 +20,7 @@ create table FichierImage (
     InfoPriseDeVue varchar(100),
     ResolutionImage number(3),
     EstPartage NUMBER(1),
-    adrMail varchar(100) REFERENCES Clients
+    Foreign key adrMail varchar(100) REFERENCES Clients
     
 )
 
@@ -29,11 +31,22 @@ create table Adresse (
     CodePostal number(5)
 ) 
 
-
 create table Impression(
     idImp varchar(100)
 
 )
+
+create table Livraison (
+    adrPointRelais varchar(100) REFERENCES Adresse ,
+    adrDomicile varchar(100) REFERENCES Adresse 
+)
+
+create table Commande (
+    dateCommande number(3),
+    NomRue varchar(100),
+    Ville varchar(100),
+    CodePostal number(5)
+) 
 
 create table tirage(
 
